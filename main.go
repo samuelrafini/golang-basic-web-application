07570dbb04ac2e55748fa67fa6ebff301bf7e613
samuel rafini <samuelrafini@gmail.com>
2019-10-30 06:04:01 +0100

    created home view

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
-func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1>First server</h1>")
+
+var homeTemplate *template.Template
+
+func home(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
+//func handlerFunc(w http.ResponseWriter, r *http.Request){
+//	w.Header().Set("Content-Type", "text/html")
+//	if r.URL.Path == "/" {
+//		fmt.Fprint(w, "<h1>First server Home</h1>")
+//	} else if r.URL.Path == "/contact"{
+//		fmt.Fprint(w, "To get in touch please send a mail to <a href=\"mailto:check@check.com\">mail.mail.com</a>")
+//	} else {
+//		w.WriteHeader(http.StatusNotFound)
+//		fmt.Fprint(w,"<h1>page can not be found</h1>")
+//	}
+//}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	http.ListenAndServe(":8080", r)
 }
