ed33a7cc14c15d72a91e56518869abbfeb45befa
samuel rafini <samuelrafini@gmail.com>
2019-10-31 08:33:47 +0100

    created more layouts and

---
+++ go.mod
@@ -0,0 +1,5 @@
+module golang-basic-web-application
+
+go 1.13
+
+require github.com/gorilla/mux v1.7.3 // indirect
--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"html/template"
+	"golang-basic-web-application/views"
 	"net/http"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
+		//err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	//err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
@@ -36,21 +40,23 @@ func contact(w http.ResponseWriter, r *http.Request){
 //}
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"views/home.gohtml",
-		"views/layouts/footer.gohtml",
-		)
-	if err != nil {
-		panic(err)
-	}
-	contactTemplate, err = template.ParseFiles(
-		"views/contact.gohtml",
-		"views/layouts/footer.gohtml",
-		)
-	if err != nil {
-		panic(err)
-	}
+	//var err error
+	//homeTemplate, err = template.ParseFiles(
+	//	"views/home.gohtml",
+	//	"views/layouts/footer.gohtml",
+	//	)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//contactTemplate, err = template.ParseFiles(
+	//	"views/contact.gohtml",
+	//	"views/layouts/footer.gohtml",
+	//	)
+	//if err != nil {
+	//	panic(err)
+	//}
+	homeView = views.NewView("default", "views/home.gohtml")
+	contactView = views.NewView("default", "views/contact.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
