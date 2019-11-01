e612252705b299cbf05cadfe5d024d1df6fecde4
samuel rafini <samuelrafini@gmail.com>
2019-11-01 19:42:59 +0100

    added schema package

--- controllers/users.go
+++ controllers/users.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/gorilla/schema"
 	"golang-basic-web-application/views"
 	"net/http"
 )
 
 type Users struct {
 	NewView *views.View
+}
+
+type SignupForm struct {
+	Email string `schema:"email"`
+	Password string `schema:"password"`
 }
 // create new users controller
 func NewUsers() *Users {
@@ -19,4 +26,20 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request){
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
+	//fmt.Fprintln(w, r.PostFormValue("email"))
+	//fmt.Fprintln(w, r.PostForm["email"])
+	//fmt.Fprintln(w, r.PostFormValue("password"))
+	//fmt.Fprintln(w, r.PostForm["password"])
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module golang-basic-web-application
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3 // indirect
+require (
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+)
